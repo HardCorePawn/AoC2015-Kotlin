6cadd1d75ebc061ad13e01aa287aefb87edceb3e
HardCorePawn <hardcorepawn69@gmail.com>
2024-12-20 11:42:36 +1300

    Day 1 - Parts 1 and 2 Solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,47 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        var floor = 0
+        input.forEach {
+            when (it) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+        }
+        return floor
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        var floor = 0
+        input.forEachIndexed { index, c ->
+            when (c) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+            if (floor == -1) return index + 1
+        }
+        return -1
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1("(())") == 0)
+    check(part1("()()") == 0)
+    check(part1("(((") == 3)
+    check(part1("(()(()(") == 3)
+    check(part1("))(((((") == 3)
+    check(part1("())") == -1)
+    check(part1("))(") == -1)
+    check(part1(")))") == -3)
+    check(part1(")())())") == -3)
+
+    check(part2(")") == 1)
+    check(part2("()())") == 5)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(input[0]).println()
+    part2(input[0]).println()
 }
